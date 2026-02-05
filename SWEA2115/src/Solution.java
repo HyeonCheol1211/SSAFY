da90344781fcb820ca2d1530de384df07d903dc0
최현철 <guscjf1211@naver.com>
2026-02-05 23:19:44 +0900

    complete SWEA2115

--- SWEA2115/src/Solution.java
+++ SWEA2115/src/Solution.java
@@ -1,37 +1,63 @@
 import java.util.*;
 import java.io.*;
+
 public class Solution {
+    static int[][] honey, memo;
+    static int N, M, C;
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		for(int test_case=1;test_case<=T;test_case++) {
-			int rst=0;
-			sb.append("#").append(test_case).append(" ");
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());//3 10
-			int M = Integer.parseInt(st.nextToken());//1 5
-			int C = Integer.parseInt(st.nextToken());//10 30
-			int[][] honey = new int[N][N];
-			for(int i=0;i<N;i++) {
-				st = new StringTokenizer(br.readLine());
-				for(int j=0;j<N;j++) {
-					honey[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-			
-			
-			
-			
-			
-			
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
 
-			
-			
-			sb.append(rst).append("\n");
-		}
-		
-	}
+        for (int test_case = 1; test_case <= T; test_case++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            C = Integer.parseInt(st.nextToken());
 
+            honey = new int[N][N];
+            memo = new int[N][N];
+
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++) {
+                    honey[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j <= N - M; j++) {
+                    memo[i][j] = dfs(i, j, 0, 0, 0);
+                }
+            }
+
+            int rst = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j <= N - M; j++) {
+                    for (int c = j + M; c <= N - M; c++) {
+                        rst = Math.max(rst, memo[i][j] + memo[i][c]);
+                    }
+                    for (int r = i + 1; r < N; r++) {
+                        for (int c = 0; c <= N - M; c++) {
+                            rst = Math.max(rst, memo[i][j] + memo[r][c]);
+                        }
+                    }
+                }
+            }
+            sb.append("#").append(test_case).append(" ").append(rst).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static int dfs(int i, int j, int cnt, int sum, int profit) {
+        if (sum > C) return 0;
+        if (cnt == M) return profit;
+
+        int val = honey[i][j + cnt];
+        return Math.max(
+                dfs(i, j, cnt + 1, sum + val, profit + val * val),
+                dfs(i, j, cnt + 1, sum, profit)
+        );
+    }
 }
