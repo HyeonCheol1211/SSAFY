d43cebab41489cec4546d2a5c4d6ca1738f4a9d1
최현철 <guscjf1211@naver.com>
2026-02-13 16:06:49 +0900

    0213

--- BOJ2825/src/Main.java
+++ BOJ2825/src/Main.java
@@ -1,9 +1,34 @@
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			System.out.println(check(br.readLine(), true));
+		}
 	}
 
+	private static int check(String str, boolean first) {
+		
+		int head = 0;
+		int tail = str.length() - 1;
+		while (tail > head) {
+			if (str.charAt(head) != str.charAt(tail) && first) {
+				if(check(str.substring(head+1, tail+1), false)==0 || check(str.substring(head, tail), false)==0) {
+					return 1;
+				}else {
+					return 2;
+				}
+			}
+			if(str.charAt(head) != str.charAt(tail) && !first) {
+				return 2;
+			}
+			head++;
+			tail--;
+		}
+		return 0;
+	}
 }
