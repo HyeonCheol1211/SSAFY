8e00ef19ca6f4c12dce3b2755050af31d446ec9d
최현철 <guscjf1211@naver.com>
2026-02-15 22:42:01 +0900

    commit

--- SWEA7206/src/Solution.java
+++ SWEA7206/src/Solution.java
@@ -2,53 +2,47 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
+    static Map<Integer, Integer> memo = new HashMap<>();
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
+
         for (int test_case = 1; test_case <= T; test_case++) {
-            sb.append("#").append(test_case).append(" ");
+            memo.clear();
             String str = br.readLine();
-            Queue<String> queue = new LinkedList<>();
-            queue.offer(str);
-            boolean plus = false;
-            int cnt=0;
-            while(!queue.isEmpty()){
-                int size = queue.size();
-                plus = false;
-                for(int i=0;i<size;i++){
-                    str = queue.poll();
-                    int total = 1;
-                    int sum = 0;
-                    for(int j=0;j<(1<<str.length()-1);j++){
-                        for(int k=0;k<str.length();k++){
-                            if((j & (1<<k)) > 0){
-                                sum += str.charAt(k) - '0';
-                                total *= sum;
-                                sum = 0;
-                            }else{
-                                sum += str.charAt(k) - '0';
-                                if(k == str.length()-1){
-                                    total *= sum;
-                                    sum = 0;
-                                }
-                            }
-                        }
-                        if(total>=10){
-                            queue.offer(String.valueOf(total));
-                            plus = true;
-                        }
-                    }
-                }
-                if(plus){
-                    cnt++;
-                }
-            }
-            sb.append(cnt);
-            sb.append("\n");
+            int rst = solve(Integer.parseInt(str));
+
+            sb.append("#").append(test_case).append(" ").append(rst).append("\n");
         }
-        System.out.println(sb);
+        System.out.print(sb);
     }
 
+    private static int solve(int num) {
+        if (num < 10) return 0;
+        if (memo.containsKey(num)) return memo.get(num);
+
+        int cnt = 0;
+        String str = String.valueOf(num);
+        int len = str.length();
+
+        for (int j = 1; j < (1 << (len - 1)); j++) {
+            int total = 1;
+            int lastIdx = 0;
+
+            for (int k = 0; k < len - 1; k++) {
+                if ((j & (1 << k)) > 0) {
+                    total *= Integer.parseInt(str.substring(lastIdx, k + 1));
+                    lastIdx = k + 1;
+                }
+            }
+            total *= Integer.parseInt(str.substring(lastIdx));
+
+            cnt = Math.max(cnt, 1 + solve(total));
+        }
+
+        memo.put(num, cnt);
+        return cnt;
+    }
 }
